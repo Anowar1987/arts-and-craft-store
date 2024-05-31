56c3e0974b8888e9edc7fd12b6125e5ef3001791
Anowar1987 <anowarhossen.iim@gmail.com>
2024-05-31 13:43:45 +0600

    banner section created

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,20 +1,25 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import Img01 from '../../../assets/Images/BannerImg/Banner01.jpg';
+import Img02 from '../../../assets/Images/BannerImg/Banner02.jpg';
+import Img03 from '../../../assets/Images/BannerImg/Banner03.jpg';
+import Img04 from '../../../assets/Images/BannerImg/Banner04.jpg';
 
 const Banner = () => {
     return (
         <Carousel>
-            <div>
-                <img src="assets/1.jpeg" />
+            <div className="h-96">
+                <img src={Img01} />
                 <p className="legend">Legend 1</p>
             </div>
-            <div>
-                <img src="assets/2.jpeg" />
-                <p className="legend">Legend 2</p>
+            <div className="h-96">
+                <img src={Img02} />
             </div>
-            <div>
-                <img src="assets/3.jpeg" />
-                <p className="legend">Legend 3</p>
+            <div className="h-96">
+                <img src={Img03} />
+            </div>
+            <div className="h-96">
+                <img src={Img04} />
             </div>
         </Carousel>
     );
